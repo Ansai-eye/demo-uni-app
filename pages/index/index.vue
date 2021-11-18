d35aad2b21f578048f8a3cf465b84f809d3b4185
Chuangshi Jiang <18251984729@163.com>
2021-11-18 09:26:37 +0800

    try cloud functions

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,57 +1,64 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+  <view class="content">
+    <image class="logo" src="/static/logo.png"></image>
+    <view class="text-area">
+      <text class="title">{{title}}</text>
+    </view>
+    <button @click="callCloudFunctions">调用云函数</button>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-			uniCloud.callFunction({
-				name: "login",
-				success(res) {
-					console.log("云函数调用成功", res);
-				}
-			})
-		},
-		methods: {
+  export default {
+    data() {
+      return {
+        title: 'Hello'
+      }
+    },
+    onLoad() {
 
-		}
-	}
+    },
+    methods: {
+      callCloudFunctions() {
+        uniCloud.callFunction({
+          name: 'login',
+          data: {
+            name: 'liming',
+            age: 23
+          },
+          success(res) {
+            console.log(res);
+          }
+        })
+      }
+    }
+  }
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+<style lang="scss">
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
+    .logo {
+      height: 200rpx;
+      width: 200rpx;
+      margin-top: 200rpx;
+      margin-left: auto;
+      margin-right: auto;
+      margin-bottom: 50rpx;
+    }
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
+    .text-area {
+      display: flex;
+      justify-content: center;
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+      .title {
+        font-size: 36rpx;
+        color: #8f8f94;
+      }
+    }
+  }
 </style>
